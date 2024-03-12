24adf8b197a88bc0c9ec2f722a8983d62afc9fc2
Logan Wasmer <loganwasmer@gmail.com>
2024-03-12 20:04:22 +0000

    start of lab05

--- bloggerApp/app.js
+++ bloggerApp/app.js
@@ -20,11 +20,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_client')));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); 
 
 app.use('/', indexRouter);
 app.use('/api', routesApi);
+
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
